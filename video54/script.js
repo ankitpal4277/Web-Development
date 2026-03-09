0d33a34da3428fb3749a1d1e0b798ce4e73f77ce
Ankit Pal <ankitpalsingh0208@gmail.com>
2026-03-09 20:34:22 +0530

    Intro to JavaScript

---
+++ video54/script.js
@@ -0,0 +1,21 @@
+alert("Hello world");
+
+console.log("Hey i am console");
+
+var a = prompt("enter your number");
+
+console.log("The number you entered is " + a);
+
+var b = confirm("Are you sure you want to submit?");
+
+if (b == true) {
+    console.log("You have submitted the form");
+}
+
+else {
+    console.log("You have cancelled the submission");
+}
+
+document.title = "New Title";
+
+document.body.style.backgroundColor = "lightblue";
