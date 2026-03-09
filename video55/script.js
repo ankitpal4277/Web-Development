7cbc6a3d089b42637547fe8ff143edccf901ede5
Ankit Pal <ankitpalsingh0208@gmail.com>
2026-03-09 23:27:59 +0530

    Javascript variables

---
+++ video55/script.js
@@ -0,0 +1,36 @@
+console.log("hello")
+
+var a = 5;
+var b = 6;
+
+console.log(a + b)
+
+console.log(typeof a , typeof b)
+
+// const c = 7;
+// c = c + 1;
+
+{
+    console.log(a)
+}
+
+let x = "Ankit Pal";
+let y = 21;
+let z = 172.5;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    name: "Ankit Pal", 
+    "job code": 4277
+}
+
+console.log(o)
+
+o.salary = 50000;
+
+console.log(o)
