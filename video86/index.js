a511155bca6cfcead5cd15121007266699340f7b
Ankit Pal <ankitpalsingh0208@gmail.com>
2026-06-27 12:21:07 +0530

    CommonJS vs EcmaScript Modules

---
+++ video86/index.js
@@ -0,0 +1,28 @@
+// const http = require('node:http');
+// const fs = require("fs")
+
+// import http from "http"
+
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/html');
+//     res.end('<h1> Hello world</h1> ');
+
+// })
+// server.listen(port, hostname, () => {
+//     console.log(`Server is running at http://${hostname}:${port}/`);
+// })
+
+// import {a} from "./mymodule.js"
+// console.log(a)
+
+// import obj from "./mymodule.js"
+// console.log(obj)
+
+const a = require("./mymodule2.js")
+console.log(a, __dirname, __filename)
+
+    
