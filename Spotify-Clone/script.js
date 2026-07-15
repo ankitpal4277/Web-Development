3ff01db38092b84e90cd73b648d049f2e0f69cb3
Ankit Pal <ankitpalsingh0208@gmail.com>
2026-07-15 13:19:22 +0530

    added the seekbar and made the play/pause button working

--- Spotify-Clone/script.js
+++ Spotify-Clone/script.js
@@ -1,40 +1,67 @@
 console.log("Lets write javascript code");
+let currentsong = new Audio()
+
+function secondsToMinutesSeconds(seconds) {
+    if(isNaN(seconds) || seconds < 0) {
+        return "Invalid input"
+    }
+
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
 
 async function getSongs() {
-    let a = await fetch("http://127.0.0.1:5500/songs/")
+    let a = await fetch("./songs/")
     let response = await a.text();
-    console.log(response);
 
     let div = document.createElement("div");
     div.innerHTML = response;
-    let as = div.getElementsByTagName("a");
+    let as = Array.from(div.getElementsByTagName("a"));
     let songs = []
-
 
     for (let i = 0; i < as.length; i++) {
         const element = as[i];
-        if (element.href.endsWith(".mp3")) {
-            songs.push(element.href.split("/songs/")[1])
+        const href = element.href;
+
+        if (href && href.toLowerCase().endsWith(".mp3")) {
+            const fileName = decodeURIComponent(new URL(href).pathname.split("/").pop());
+            songs.push(fileName);
         }
     }
 
-    return songs  
+    return songs
 }
 
-async function main(){
+const playMusic = (track) => {
+    console.log("Trying to play:", track)
+    currentsong.src = `./songs/${encodeURIComponent(track)}`;
+    console.log("Final src:", currentsong.src);
+    currentsong.load();
+    currentsong.play().catch(err => console.error("Playback failed:", err));
+    play.src = "./img/pause.svg"
+    document.querySelector(".songinfo").innerHTML = track;
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
+}
 
-    let currentsong;
+async function main() {
+
+
     // Get the list of all the songs
     let songs = await getSongs()
-    console.log(songs);
+    currentsong.src = songs[0]
 
     // show all the songs in the playlist
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
     for (const song of songs) {
-        songUL.innerHTML = songUL.innerHTML + `<li>
+        songUL.innerHTML += `<li>
                             <img class="invert" src="./img/music.svg" alt="">
                             <div class="info">
-                                 <div>${song.replaceAll("%20", "")}</div>
+                                 <div>${song}</div>
                                  <div>Me</div>
                             </div>
                             <div class="playnow">
@@ -45,11 +72,32 @@ async function main(){
     }
 
     // Attach an eventlistener to each song 
-    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e =>{
-        console.log(e.target.);
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", element => {
+            console.log(e.querySelector(".info").firstElementChild.innerHTML);
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
+        })
+
     });
 
-} 
+    // Attach an event listener to play, next and previous
+    play.addEventListener("click", () => {
+        if(currentsong.paused) {
+            currentsong.play()
+            play.src = "./img/pause.svg"
+        }
+        else {
+            currentsong.pause()
+            play.src = "./img/play.svg";
+        }
+    })
+
+    // Listen for timeupdate event 
+    currentsong.addEventListener("timeupdate", ()=>{
+        console.log(currentsong.currentTime, currentsong.duration);
+        document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentsong.currentTime)}/${secondsToMinutesSeconds(currentsong.duration) }`
+    })
+}
 
 main()
 
