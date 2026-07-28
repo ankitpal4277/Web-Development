d114539079f647a51a04b1ab3cf56b758cdbc9ab
Ankit Pal <ankitpalsingh0208@gmail.com>
2026-07-28 14:24:47 +0530

    added a volume seekbar

--- Spotify-Clone/script.js
+++ Spotify-Clone/script.js
@@ -1,9 +1,10 @@
 console.log("Lets write javascript code");
+let songs;
 let currentsong = new Audio()
 
 function secondsToMinutesSeconds(seconds) {
     if (isNaN(seconds) || seconds < 0) {
-        return "Invalid input"
+        return "00:00";
     }
 
     const minutes = Math.floor(seconds / 60);
@@ -57,7 +58,7 @@ async function main() {
 
 
     // Get the list of all the songs
-    let songs = await getSongs()
+    songs = await getSongs()
     playMusic(songs[0], true)
 
     // show all the songs in the playlist
@@ -101,26 +102,54 @@ async function main() {
     currentsong.addEventListener("timeupdate", () => {
         console.log(currentsong.currentTime, currentsong.duration);
         document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentsong.currentTime)} / ${secondsToMinutesSeconds(currentsong.duration)}`
-        document.querySelector(".circle").style.left = (currentsong.currentTime/ currentsong.duration) * 100 + "%";
+        document.querySelector(".circle").style.left = (currentsong.currentTime / currentsong.duration) * 100 + "%";
     })
 
     // Add an event listener to the seekbar   
-    document.querySelector(".seekbar").addEventListener("click", e=>{
-        let percent = (e.offsetX/e.target.getBoundingClientRect().width) * 100;
+    document.querySelector(".seekbar").addEventListener("click", e => {
+        let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
         document.querySelector(".circle").style.left = percent + "% ";
-        currentsong.currentTime = ((currentsong.duration)* percent)/100
-    })  
-    
+        currentsong.currentTime = ((currentsong.duration) * percent) / 100
+    })
+
     // Add an event listener for hamburger 
-    document.querySelector(".hamburger").addEventListener("click", ()=>{
+    document.querySelector(".hamburger").addEventListener("click", () => {
         document.querySelector(".left").style.left = "0"
-    }) 
-    
+    })
+
 
     // Add an event listener for close button 
-    document.querySelector(".close").addEventListener("click", ()=>{
+    document.querySelector(".close").addEventListener("click", () => {
         document.querySelector(".left").style.left = "-120%"
-    }) 
+    })
+
+    // Add an event listener to previous 
+    previous.addEventListener("click", () => {
+        let currentFile = decodeURIComponent(currentsong.src.split("/").slice(-1)[0]);
+        let index = songs.indexOf(currentFile);
+
+        if (index - 1 >= 0) {
+            playMusic(songs[index - 1]);
+        }
+    })
+
+    // Add an event listener to next 
+    next.addEventListener("click", () => {
+        currentsong.pause();
+        let currentFile = decodeURIComponent(currentsong.src.split("/").slice(-1)[0]);
+        let index = songs.indexOf(currentFile);
+
+        if (index + 1 < songs.length) {
+            playMusic(songs[index + 1]);
+        }
+    })
+
+    // Add an event to volume 
+    document.querySelector(".range").getElementsByTagName("input")[0].addEventListener("change", ()=>{
+        console.log(e, e.target, e.target.value)
+        currentsong.volume = parseInt(e.target.value)/100
+    })
+
 }
 
 main()
